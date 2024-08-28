389b1eea0b57adfa71010d83dd1a87b9f0a353f0
nicolechuah <nicole.chuahxl@gmail.com>
2024-08-28 21:27:54 +0800

    Finished with a few bugs

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from app.config import Config
-# TODO: import flask_sqlalchemy
-# TODO: import flask_login
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from os import path
 
 
 
 # TODO: declare sqlalchemy db here
+db = SQLAlchemy()
 csrf = CSRFProtect()
 
 
@@ -16,18 +18,27 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     
     
-    # from .models import Users
+    from .models import Users, Expenses
 
     with app.app_context():
         # TODO: initialise sqlalchemy db here
-        
+        db.init_app(app)
         # TODO: create sqlalchemy db file
-        
+        if not path.exists(app.config['DATABASE_NAME']):
+            db.create_all()
+            print('Created Database!')
         csrf.init_app(app)
 
 
         # TODO: initialise loginmanager
+        login_manager = LoginManager()
+        login_manager.login_view = 'auth.login'
+        login_manager.init_app(app)
 
+
+        @login_manager.user_loader
+        def load_user(id):
+            return Users.query.get(id)
 
         from app.auth import auth
         from app.views import views
--- app/forms.py
+++ app/forms.py
@@ -2,30 +2,30 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, HiddenField, SelectField, FloatField, HiddenField, PasswordField
 from wtforms.fields import EmailField, DateField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError, Optional
-# from .models import Users
+from .models import Users
 import re
 
 
-# class Login(FlaskForm):
-#     email = EmailField('Email', validators=[DataRequired(), Email(message='Invalid Email')]) 
-#     password = PasswordField('Password', validators=[DataRequired()])
-#     submit = SubmitField('Log In')
+class Login(FlaskForm):
+     email = EmailField('Email', validators=[DataRequired(), Email(message='Invalid Email')]) 
+     password = PasswordField('Password', validators=[DataRequired()])
+     submit = SubmitField('Log In')
 
 
-# class SignUp(FlaskForm):
-#     name = StringField('First Name', validators=[Optional(), Length(min=2, max=20)]) 
-#     email = EmailField('Email*', validators=[DataRequired(), Email(message='Invalid Email')]) 
-#     password = PasswordField('Password*', validators=[DataRequired(), Length(min=8, max=24)])
-#     confirm_password = PasswordField('Confirm Password*', validators=[DataRequired(), EqualTo(fieldname="password", message="Passwords must match")])
-#     submit = SubmitField('Sign Up')
+class SignUp(FlaskForm):
+     name = StringField('First Name', validators=[Optional(), Length(min=2, max=20)]) 
+     email = EmailField('Email*', validators=[DataRequired(), Email(message='Invalid Email')]) 
+     password = PasswordField('Password*', validators=[DataRequired(), Length(min=8, max=24)])
+     confirm_password = PasswordField('Confirm Password*', validators=[DataRequired(), EqualTo(fieldname="password", message="Passwords must match")])
+     submit = SubmitField('Sign Up')
 
-#     def validate_email(self, email):
-#         user = Users.query.filter_by(email=email.data.lower()).first()
-#         if user:
-#             raise ValidationError("This email is already in use, please use a different one.")
-#     def validate_password(self, password):
-#         if not re.fullmatch("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$", password.data):
-#             raise ValidationError("Password needs to contain at least one letter, number, and special character.")
+     def validate_email(self, email):
+         user = Users.query.filter_by(email=email.data.lower()).first()
+         if user:
+             raise ValidationError("This email is already in use, please use a different one.")
+     def validate_password(self, password):
+         if not re.fullmatch("^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$", password.data):
+             raise ValidationError("Password needs to contain at least one letter, number, and special character.")
 
 
 Type_of_expense =["Personal", "House", "Transport", "Pets", "Miscellaneous"]
